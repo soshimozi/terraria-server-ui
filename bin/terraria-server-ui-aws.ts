eb3a06f0a7735b1d4d34c237a21b6799894afe36
Scott McCain <mccains@pacden.com>
2023-06-17 22:24:14 -0700

    Modified so linter doesn't complain

--- bin/terraria-server-ui-aws.ts
+++ bin/terraria-server-ui-aws.ts
@@ -1,1 +1,20 @@
 #!/usr/bin/env node
+import 'source-map-support/register'
+require('dotenv').config()
+const crypto = require('crypto')
+
+import * as cdk from 'aws-cdk-lib'
+
+// import { TestCdkServerStack } from '../lib/test-cdk-aws-stack'
+
+// const keyName = process.env.KEYNAME ?? ''
+
+// if(!keyName) {
+//     throw 'Should have the name of your IAM keypair in the .env file'
+// }
+
+// const app = new cdk.App()
+// new TestCdkServerStack(app, 'Default', {
+//     keyName,
+//     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// })
